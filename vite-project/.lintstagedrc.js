51ba858d2dbb581a0cd79a92d769202b9fc83e31
Kseniya <pen.cil@mail.ru>
2023-10-27 16:29:47 +0500

    refactor: husky

--- vite-project/.lintstagedrc.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  "*.{js,ts,tsx}": [
-    "eslint . --ext ts,tsx --report-unused-disable-directives --max-warnings 0",
-    "prettier --write --ignore-path .gitignore .",
-  ],
-};
