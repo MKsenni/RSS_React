46fe66e823753f7e37a11c54275510324b05b850
Kseniya <pen.cil@mail.ru>
2023-10-30 01:40:31 +0500

    feat: add spinner for load data

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -2,12 +2,14 @@ import { ReactNode } from 'react';
 import './App.css';
 import React from 'react';
 import { Description, getPeople, searchPeople } from './api/actions';
+import { Spinner } from './components/spinner';
 
 type AppProps = Record<string, never>;
 
 type AppState = {
   searchWord: string;
   searchResult?: Description;
+  isLoading: boolean;
 };
 
 class App extends React.Component<AppProps, AppState> {
@@ -16,6 +18,7 @@ class App extends React.Component<AppProps, AppState> {
     this.state = {
       searchWord: localStorage.getItem('searchWord') || '',
       searchResult: undefined,
+      isLoading: false,
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSearch = this.handleSearch.bind(this);
@@ -23,12 +26,14 @@ class App extends React.Component<AppProps, AppState> {
 
   async componentDidMount(): Promise<void> {
     const searchWord = this.state.searchWord;
+    this.setState({ isLoading: true });
     if (searchWord) {
       const people = await searchPeople(searchWord);
       this.setState({ searchResult: people });
     } else {
       await this.startPage();
     }
+    this.setState({ isLoading: false });
   }
 
   componentDidUpdate(/*prevProps: Readonly<AppProps>, prevState: Readonly<AppState>, snapshot?: any*/): void {}
@@ -51,13 +56,18 @@ class App extends React.Component<AppProps, AppState> {
   async handleSearch(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
     const searchWord: string = this.state.searchWord.toUpperCase().trim();
+    this.setState({ isLoading: true });
     const people = await searchPeople(searchWord);
     this.setState({ searchResult: people });
     localStorage.setItem('searchWord', searchWord);
+    this.setState({ isLoading: false });
   }
   render(): ReactNode {
     return (
       <>
+        <h1 className="title">
+          Star Wars <p className="subtitle">: heroes</p>
+        </h1>
         <section>
           <form onSubmit={this.handleSearch}>
             <input
@@ -68,24 +78,34 @@ class App extends React.Component<AppProps, AppState> {
             <input type="submit" value="search" />
           </form>
         </section>
-        <section>
-          <div className="cards">
-            {this.state.searchResult &&
-              this.state.searchResult?.map((person, index) => {
-                return (
-                  <div className="card" key={index}>
-                    <span className="card-name">Name: {person.name}</span>
-                    <span className="card-gender">Gender: {person.gender}</span>
-                    <span className="card-birth">
-                      Year: {person.birth_year}
-                    </span>
-                    <span className="card-mass">Mass: {person.mass}</span>
-                    <span className="card-height">Height: {person.height}</span>
-                  </div>
-                );
-              })}
-          </div>
-        </section>
+        {this.state.isLoading ? (
+          <Spinner />
+        ) : (
+          <section>
+            <div className="cards">
+              {this.state.searchResult &&
+                this.state.searchResult?.map((person, index) => {
+                  return (
+                    <div className="card" key={index}>
+                      <div className="text-container">
+                        <span className="card-name">Name: {person.name}</span>
+                        <span className="card-gender">
+                          Gender: {person.gender}
+                        </span>
+                        <span className="card-birth">
+                          Year: {person.birth_year}
+                        </span>
+                        <span className="card-mass">Mass: {person.mass}</span>
+                        <span className="card-height">
+                          Height: {person.height}
+                        </span>
+                      </div>
+                    </div>
+                  );
+                })}
+            </div>
+          </section>
+        )}
       </>
     );
   }
---
+++ vite-project/src/components/spinner.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { ReactNode } from 'react';
+
+export class Spinner extends React.Component {
+  render(): ReactNode {
+    return (
+      <>
+        <img
+          width={60}
+          src="/spinner.gif"
+          alt="loading"
+          style={{ margin: '20px' }}
+        />
+      </>
+    );
+  }
+}
