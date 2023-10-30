d8d5b74ca7ea5f15213a38969a6c37e0c46ba2d2
Kseniya <pen.cil@mail.ru>
2023-10-30 06:17:45 +0500

    fix: fix deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/RSS_react/',
+  base: '/RSS_React/',
   server: {
     open: true,
   },
