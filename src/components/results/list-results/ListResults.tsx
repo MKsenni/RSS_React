1454498a8e40bb7e77b80e63cc36e18a2e20c57a
Kseniya <pen.cil@mail.ru>
2023-11-15 01:30:07 +0500

    feat: add list of results test

--- src/components/results/list-results/ListResults.tsx
+++ src/components/results/list-results/ListResults.tsx
@@ -1,15 +1,15 @@
 import style from './listResults.module.css';
-import { NavLink, Outlet, useLocation, useNavigation } from 'react-router-dom';
+import { Outlet, useNavigation } from 'react-router-dom';
 import Spinner from '../../spiner/Spinner';
 import { useContext, useEffect, useState } from 'react';
 import { ResultsPeopleContext } from '../../../context';
+import { PeopleResponse } from '../../../services/actions';
+import CardResults from './card-results/CardResults';
 
 export default function ListResults() {
-  const people = useContext(ResultsPeopleContext);
+  const people: PeopleResponse | null = useContext(ResultsPeopleContext);
   const [searchResult, setSearchResult] = useState(people?.results);
   const navigation = useNavigation();
-
-  const { search } = useLocation();
 
   useEffect(() => {
     setSearchResult(people?.results);
@@ -21,18 +21,9 @@ export default function ListResults() {
         <nav className={style.navigation}>
           {searchResult?.length ? (
             <ul className={style.cards}>
-              {searchResult.map((hero, index) => (
-                <li key={index} className={style.link}>
-                  <NavLink
-                    className={({ isActive, isPending }) =>
-                      isActive ? 'active' : isPending ? 'pending' : ''
-                    }
-                    to={`details/${hero.name}` + search}
-                  >
-                    {hero.name ? <>{hero.name}</> : <span>No Name</span>}
-                  </NavLink>
-                </li>
-              ))}
+              {searchResult.map((hero, index) => {
+                return <CardResults key={index} hero={hero} />;
+              })}
             </ul>
           ) : (
             <span>No Results</span>
---
+++ src/components/results/list-results/listResults.test.tsx
@@ -0,0 +1,44 @@
+import '@testing-library/jest-dom';
+import { ResultsPeopleContext } from '../../../context';
+import ListResults from './ListResults';
+import { cleanup, render, screen } from '@testing-library/react';
+import { mockResults } from '../../../data/data-mocks';
+import { MemoryRouter } from 'react-router-dom';
+
+const mockUseNavigate = jest.fn().mockReturnValue('');
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useNavigate: () => mockUseNavigate,
+  useLocation: jest.fn().mockReturnValue({
+    pathname: 'details/Luke Skywalker',
+    search: '',
+  }),
+  useNavigation: jest.fn().mockReturnValue({
+    state: 'loading',
+  }),
+}));
+afterEach(cleanup);
+
+describe('ListResults component', () => {
+  it('Check that an appropriate message is displayed if no cards are present', () => {
+    render(
+      <ResultsPeopleContext.Provider value={null}>
+        <ListResults />
+      </ResultsPeopleContext.Provider>
+    );
+
+    expect(screen.getByText('No Results')).toBeInTheDocument();
+  });
+  it('Verify that the component renders the specified number of cards', () => {
+    const lengthResults = 6;
+    render(
+      <MemoryRouter>
+        <ResultsPeopleContext.Provider value={mockResults}>
+          <ListResults />
+        </ResultsPeopleContext.Provider>
+      </MemoryRouter>
+    );
+
+    expect(screen.getAllByRole('listitem')).toHaveLength(lengthResults);
+  });
+});
