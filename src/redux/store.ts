e088cd2f305c9d2717caef0f5984fd5158a84858
Kseniya <pen.cil@mail.ru>
2023-12-04 03:03:50 +0500

    refactor: change structure

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,16 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import dataReducer from './slices/dataSlice';
 import countrySliceReducer from './slices/countrySlice';
-import { peopleApi } from '../services/peopleApi';
 
 export const store = configureStore({
   reducer: {
     data: dataReducer,
     countries: countrySliceReducer,
-    [peopleApi.reducerPath]: peopleApi.reducer,
   },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(peopleApi.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
